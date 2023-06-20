ea752841da78b77eef241ef9425e95084273d554
omranali2111 <aljabri1234@hotmail.com>
2023-06-20 11:22:25 +0400

    Polymorphism

--- c-sharp-Inheritance-tasks/Program.cs
+++ c-sharp-Inheritance-tasks/Program.cs
@@ -6,20 +6,24 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Student student1 = new Student("omran", 27, 14125555, "commEng");
-        student1.printData();
+        //Student student1 = new Student("omran", 27, 14125555, "commEng");
+        //student1.printData();
 
-        Console.WriteLine("===============================================");
+        //Console.WriteLine("===============================================");
 
-        Rectangle1 rectangle = new Rectangle1("red", 0, 2.4, 2.5);
+        //Rectangle1 rectangle = new Rectangle1("red", 0, 2.4, 2.5);
 
-        Console.WriteLine("===============================================");
+        //Console.WriteLine("===============================================");
 
-        Dog dog1 = new Dog("jack",2,"breed","wof");
-        Dog dog2 = new Dog("jacky", 2, "breed", "wof");
-        dog1.makeSound();
-        dog1.getSound();
-        dog1.doSomething();
-        Console.WriteLine(dog1.Equals(dog2));
+        //Dog dog1 = new Dog("jack",2,"breed","wof");
+        //Dog dog2 = new Dog("jacky", 2, "breed", "wof");
+        //dog1.makeSound();
+        //dog1.getSound();
+        //dog1.doSomething();
+        //Console.WriteLine(dog1.Equals(dog2));
+
+        Animal animal = new Dog("jack", 2, "breed", "wof");// 
+       //type   variable     object
+        animal.makeSound();
     }
 }
